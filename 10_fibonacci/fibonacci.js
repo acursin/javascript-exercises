6a570de13cc68ad64d25a81f9789376098fbf0ac
alex <109973612+acursin@users.noreply.github.com>
2024-07-22 16:52:30 -0400

    Complete exercise without solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(limit) {
+    if (limit < 0) return "OOPS";
+    limit = +limit;
+    if (limit === 0) return 0;
 
+    let previousNums = [0, 1];
+    let currentNum = 1;
+
+    for (let i = 1; i < limit; i++) {
+        currentNum = previousNums[0] + previousNums[1];
+        previousNums[0] = previousNums[1];
+        previousNums[1] = currentNum;
+    }
+
+    return currentNum;
 };
 
 // Do not edit below this line
