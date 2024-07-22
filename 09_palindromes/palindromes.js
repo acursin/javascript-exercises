4d5e16c5bb889b4564a573ddf1b1df5a80c41f23
alex <109973612+acursin@users.noreply.github.com>
2024-07-22 16:19:16 -0400

    Complete exercise without solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const palindrome = str
+        .toLowerCase()
+        .split('')
+        .filter(char => (char.toUpperCase() != char.toLowerCase()) || +char)
 
+    return palindrome.join('') === palindrome.toReversed().join('');
 };
 
 // Do not edit below this line
