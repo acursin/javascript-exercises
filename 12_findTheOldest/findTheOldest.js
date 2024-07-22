9c2196b0695ddae68f6e2c57b7dbf78534e85e53
alex <109973612+acursin@users.noreply.github.com>
2024-07-22 18:00:25 -0400

    Complete exercise without solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    for (let person of people) {
+        if (!('yearOfDeath' in person)) {
+            person['yearOfDeath'] = new Date().getFullYear();
+        }
+    }
 
+    return people.reduce((oldestPerson, person) => {
+        if (person.yearOfDeath - person.yearOfBirth > oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) {
+            oldestPerson = person;
+        }
+        return oldestPerson;
+    });
 };
 
 // Do not edit below this line
